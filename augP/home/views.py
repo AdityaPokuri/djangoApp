ae84bc6a0e2901401decdfda6b8bedc9cbe28d14
aditya <mpokuri@bgsu.edu>
2019-08-24 19:52:00 -0400

    inital commit

---
+++ augP/home/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django import http
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    dict = {'template_var': 'this is var'}
+    return render(request, 'index.html', context = dict)
+
+
+def about(request):
+    return HttpResponse("What the about")
+
+
+def gallery(request):
+    return HttpResponse("What the gallery")
